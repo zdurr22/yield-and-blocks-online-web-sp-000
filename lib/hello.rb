39c06eaa54f27fa51da1fe32c9d1381dd67a9a94
Zachary Durr <zdurr22@gmail.com>
2020-10-01 18:20:00 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,11 @@
 def hello_t(array)
-array.collect { |name| puts name }
-array
-end
+  i = 0
 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+
+  array
+end
 # call your method here!
